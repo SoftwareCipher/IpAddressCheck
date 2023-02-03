916061c02b64edb9c2bcd95d8b2c3225f1410b93
Observer <kirillkuznetsov181@gmail.com>
2023-02-03 09:58:59 +0200

    start project

---
+++ src/main/java/com/address/server/IpAddressCheckApplication.java
@@ -0,0 +1,12 @@
+package com.address.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAddressCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpAddressCheckApplication.class, args);
+    }
+}
---
+++ src/test/java/com/address/server/IpAddressCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.address.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpAddressCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
